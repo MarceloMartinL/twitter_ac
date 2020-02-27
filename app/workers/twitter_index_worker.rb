a41a0532e153bb364055dcae675aa585dde20da7
Marcelo Martin <marcelo18a@msn.com>
2020-02-27 13:23:04 -0300

    add env variables for postgreSQL, connect twitter API

--- app/workers/twitter_index_worker.rb
+++ app/workers/twitter_index_worker.rb
@@ -1,12 +1,18 @@
+require 'sidekiq-scheduler'
+
 class TwitterIndexWorker
   include Sidekiq::Worker
 
   def perform
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "YOUR_CONSUMER_KEY"
-      config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-      config.access_token        = "YOUR_ACCESS_TOKEN"
-      config.access_token_secret = "YOUR_ACCESS_SECRET"
+      config.consumer_key        = ENV['TWITTER_API_KEY']
+      config.consumer_secret     = ENV['TWITTER_SECRET_KEY']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_SECRET_TOKEN']
+    end
+
+    client.search('#chiledesperto').take(3).each do |tweet|
+      puts tweet.text
     end
   end
 end
