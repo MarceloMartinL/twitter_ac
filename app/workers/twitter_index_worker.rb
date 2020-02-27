1ec95aabca598408c26ccf8309f5b0b2a2b56fa6
Marcelo Martin <marcelo18a@msn.com>
2020-02-27 17:44:27 -0300

    add sidekiq and redis to docker-compose, set cron every 5 seconds

--- app/workers/twitter_index_worker.rb
+++ app/workers/twitter_index_worker.rb
@@ -4,15 +4,16 @@ class TwitterIndexWorker
   include Sidekiq::Worker
 
   def perform
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['TWITTER_API_KEY']
-      config.consumer_secret     = ENV['TWITTER_SECRET_KEY']
-      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-      config.access_token_secret = ENV['TWITTER_SECRET_TOKEN']
-    end
+    # client = Twitter::REST::Client.new do |config|
+    #   config.consumer_key        = ENV['TWITTER_API_KEY']
+    #   config.consumer_secret     = ENV['TWITTER_SECRET_KEY']
+    #   config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+    #   config.access_token_secret = ENV['TWITTER_SECRET_TOKEN']
+    # end
 
-    client.search('#chiledesperto').take(3).each do |tweet|
-      puts tweet.text
-    end
+    # client.search('#chiledesperto').take(3).each do |tweet|
+    #   puts tweet.text
+    # end
+    puts "WALALOOOOOOOOOOOOOOOO"
   end
 end
